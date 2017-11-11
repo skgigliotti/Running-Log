8ff0785f119f172064e10e65e1f15d921231cf97
skgigliotti <31643797+skgigliotti@users.noreply.github.com>
2017-11-10 17:47:55 -0800

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,12 @@
+class Run{
+    private int tMin;
+    private int tSec;
+    private double distance;
+
+    public Run(double dist, int mins, int secs){
+
+       tMin = mins;
+       tSec = secs;
+       distance = dist;
+    }
+}
